8d54bc3f8ce0b3b8aba71db20210f6e0e4850b02
Luiza <luizahelena.lsantos@gmail.com>
2025-03-15 16:14:10 -0300

    endpoint /estatistica para GET

--- src/main/java/com/desafio/itau/controllers/EstatisticasController.java
+++ src/main/java/com/desafio/itau/controllers/EstatisticasController.java
@@ -1,4 +1,23 @@
 package com.desafio.itau.controllers;
 
+import com.desafio.itau.business.services.EstatisticasService;
+import com.desafio.itau.controllers.dtos.EstatisticasResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/estatistica")
 public class EstatisticasController {
+    private final EstatisticasService estatisticasService;
+
+    @GetMapping
+    public ResponseEntity<EstatisticasResponseDTO> obterEstatisticas() {
+        EstatisticasResponseDTO estatisticas = estatisticasService.calcularEstatisticas(600);
+
+        return ResponseEntity.ok(estatisticas);
+    }
 }
