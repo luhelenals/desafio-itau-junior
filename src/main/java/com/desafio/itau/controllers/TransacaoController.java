74d4b610df8985a8d856f821c50548bc7b30d3ce
Luiza <luizahelena.lsantos@gmail.com>
2025-03-15 16:07:54 -0300

    endpoint /transacao para POST e DELETE

--- src/main/java/com/desafio/itau/controllers/TransacaoController.java
+++ src/main/java/com/desafio/itau/controllers/TransacaoController.java
@@ -1,4 +1,29 @@
-package com.desafio.itau.controllers.dtos;
+package com.desafio.itau.controllers;
 
+import com.desafio.itau.business.services.TransacaoService;
+import com.desafio.itau.controllers.dtos.TransacaoRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/transacao")
 public class TransacaoController {
+    private final TransacaoService transacaoService;
+
+    // POST nova transação
+    @PostMapping
+    public ResponseEntity<Void> adicionarTransacao(@RequestBody TransacaoRequestDTO transacaoRequestDTO) {
+        transacaoService.adicionarTransacao(transacaoRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    // DELETE todas as transações
+    @DeleteMapping
+    public ResponseEntity<Void> limparTransacoes() {
+        transacaoService.limparTransacoes();
+        return ResponseEntity.ok().build();
+    }
 }
